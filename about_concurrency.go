d0865a89375d74577ff64b179a087f3b60a297f1
warmans <stefan.warman@switchconcepts.com>
2015-02-11 22:09:38 +0000

    Fixed failing tests

--- about_concurrency.go
+++ about_concurrency.go
@@ -1,5 +1,7 @@
 package go_koans
 
+import "fmt"
+
 func isPrimeNumber(possiblePrime int) bool {
 	for underPrime := 2; underPrime < possiblePrime; underPrime++ {
 		if possiblePrime%underPrime == 0 {
@@ -11,7 +13,12 @@ func isPrimeNumber(possiblePrime int) bool {
 
 func findPrimeNumbers(channel chan int) {
 	for i := 2; ; /* infinite loop */ i++ {
-		// your code goes here
+
+		fmt.Print(i)
+
+		if isPrimeNumber(i) {
+			channel <- i
+		}
 
 		assert(i < 100) // i is afraid of heights
 	}
@@ -20,9 +27,7 @@ func findPrimeNumbers(channel chan int) {
 func aboutConcurrency() {
 	ch := make(chan int)
 
-	assert(__delete_me__) // concurrency can be almost trivial
-	// your code goes here
-
+	go findPrimeNumbers(ch)
 	assert(<-ch == 2)
 	assert(<-ch == 3)
 	assert(<-ch == 5)
